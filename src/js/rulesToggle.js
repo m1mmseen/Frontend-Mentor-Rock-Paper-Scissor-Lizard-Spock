1dc9ee44826a4379f4ace3599e4855cc5a2f54be
m1mmseen <christin.hanke@web.de>
2023-12-01 14:59:01 +0100

    Scss refactored

--- src/js/rulesToggle.js
+++ src/js/rulesToggle.js
@@ -2,9 +2,9 @@ const rules = document.querySelector(".rule-board");
 const rulesToggle = document.querySelector(".rulesToggle");
 const closeRules = document.getElementById("close");
 
-rulesToggle.addEventListener("click", e => {
+rulesToggle.addEventListener("click", () => {
     rules.classList.add("visible");
 })
-closeRules.addEventListener("click", e => {
+closeRules.addEventListener("click", () => {
     rules.classList.remove("visible");
 })
